a3e15e478da53fdbb3643ad1ce46288afec5975f
MJ <maitreyjukar@gmail.com>
2023-04-19 15:31:18 -0600

    [FE-0001] Add recent search component

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,12 +1,16 @@
+
 <template>
     <div class="search-bar-holder">
         <v-text-field density="compact" variant="solo" label="Search documents" append-inner-icon="mdi-magnify"
             single-line clearable hide-details v-model="searchText" @click:append-inner="onSearch"
             @keyup.enter="onSearch"></v-text-field>
+        <RecentSearches @search="onRecentSearch" />
     </div>
 </template>
   
 <script>
+import { addWordToLocalStorage } from '../common/helpers';
+import RecentSearches from './RecentSearches.vue';
 export default {
     props: {
         navigate: {
@@ -27,13 +31,23 @@ export default {
     methods: {
         onSearch() {
             if (this.searchText) {
-                this.$emit('search', this.searchText)
-                if (this.navigate) {
-                    this.$router.push(`/search/?text=${this.searchText}`)
-                }
+                this.performSearch(this.searchText);
             }
         },
+        onRecentSearch(word) {
+            this.performSearch(word);
+            this.searchText = word;
+        },
+        performSearch(text) {
+            const searchText = text.trim();
+            addWordToLocalStorage(searchText);
+            this.$emit("search", searchText);
+            if (this.navigate) {
+                this.$router.push(`/search/?text=${searchText}`);
+            }
+        }
     },
+    components: { RecentSearches }
 }
 </script>
 <style scoped>
