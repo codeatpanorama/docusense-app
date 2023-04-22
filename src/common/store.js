937fc0031490a80dacad9ba5b7be5df705d94a30
MJ <maitreyjukar@gmail.com>
2023-04-22 14:19:10 -0600

    WIP: Store

---
+++ src/common/store.js
@@ -0,0 +1,16 @@
+import {reactive, readonly} from 'vue';
+
+export class Store {
+    constructor() {
+      let data = this.data()
+      this.setup(data)
+      this.state = reactive(data)
+    }
+  
+    setup(data) {}
+  
+    getState() {
+      return readonly(this.state)
+    }
+  }
+  
