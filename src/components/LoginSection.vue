38614c6eb87800748461f0132b5d16ab200e6ba9
MJ <maitreyjukar@gmail.com>
2023-04-29 17:12:32 -0600

    Add padding for login wrapper

--- src/components/LoginSection.vue
+++ src/components/LoginSection.vue
@@ -119,7 +119,7 @@ export default {
 .login-wrapper {
     border: 2px solid var(--color-border-subtle);
     border-radius: 8px;
-    min-width: 400px;
+    width: 400px;
     overflow: hidden;
     
     @include for-phone-only {
